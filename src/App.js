7656181b195eb4591221ba35f5a528268195fdd1
Sanfeng Wang <wangsanfeng1998@gmail.com>
2019-10-02 16:56:55 -0500

    static products with images working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import 'rbx/index.css';
+import { Column, Container, Navbar} from 'rbx';
+import Product from './Components/Card';
 
 const App = () => {
   const [data, setData] = useState({});
@@ -13,10 +16,24 @@ const App = () => {
   }, []);
 
   return (
-    <ul>
-      {products.map(product => <li key={product.sku}>{product.title}</li>)}
-    </ul>
+      <Container as='div' style={ {width: '100%', paddingTop: '20px'} }>
+        <Navbar fixed='top' as='div' style={ {paddingLeft: '60px', paddingRight: '50px', paddingTop: '10px'} }>
+        <Navbar.Brand>
+          <Navbar.Item>
+              <h1 style={{fontSize: '24px'}}><strong>Catalog</strong></h1>
+              </Navbar.Item>
+          </Navbar.Brand>
+        </Navbar>
+        <Column.Group multiline>
+          <Column size='full'>
+          </Column>
+          {products.map(product =>
+            <Column size='one-third'>
+              <Product product={ product } />
+            </Column>
+          )}
+        </Column.Group>
+      </Container>
   );
 };
-
 export default App;
